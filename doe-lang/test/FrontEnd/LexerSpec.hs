5e2d2e4ba49ee82e1d1d71f8696077c7020e1ea7
Luis Diaz <ldiazn98@gmail.com>
2021-06-03 13:13:01 -0400

    added more tests

--- doe-lang/test/FrontEnd/LexerSpec.hs
+++ doe-lang/test/FrontEnd/LexerSpec.hs
@@ -14,15 +14,25 @@ atlaIntro = "Water. Earth. Fire. Air. Long ago, the four nations lived together 
 spec :: Spec
 spec = do 
     describe "scanTokens" $ do
-        it "tokenize string and parse errors if finds some" $ do
+        it "ignores comments" $ do
             L.scanTokens "-- asdaa" `shouldBe` ([], []) 
 
         it "Ignore ATLA part of the intro" $ do
-            L.scanTokens atlaIntro `shouldBe` ([], []) 
+            L.scanTokens atlaIntro `shouldBe` ([], [])         
 
-        it "tokenize string of simple example" $ do
-            L.scanTokens "aang is 5" `shouldBe` ([],[TK.TKid {TK.pos = (1,1), TK.id = "aang"},TK.TKis {TK.pos = (1,6)},TK.TKint {TK.pos = (1,9), TK.numI = 5}])
-        
         it "tokenize bad id and return error" $ do
-            L.scanTokens "_aang is 5" `shouldBe` ([E.LexerError {E.pos = (1,1), E.errorMessage = "Unexpected element: _"}],[TK.TKid {TK.pos = (1,2), TK.id = "aang"},TK.TKis {TK.pos = (1,7)},TK.TKint {TK.pos = (1,10), TK.numI = 5}])
+            L.scanTokens "0sand_storm" `shouldBe` ([E.LexerError {E.pos = (1,1), E.errorMessage = "Unexpected element: 0sand_storm"}],[])
 
+        it "tokenize valid variable declaration & ids" $ do
+            L.scanTokens "bender aang of air" `shouldBe` ([],[TK.TKbender {TK.pos = (1,1)},TK.TKid {TK.pos = (1,8), TK.id = "aang"},TK.TKof {TK.pos = (1,13)},TK.TKair {TK.pos = (1,16)}])
+            L.scanTokens "eternal bender aang of air" `shouldBe` ([],[TK.TKeternal {TK.pos = (1,1)},TK.TKbender {TK.pos = (1,9)},TK.TKid {TK.pos = (1,16), TK.id = "aang"},TK.TKof {TK.pos = (1,21)},TK.TKair {TK.pos = (1,24)}])
+            L.scanTokens "sand_storm" `shouldBe` ([],[TK.TKid {TK.pos = (1,1), TK.id = "sand_storm"}])
+
+        it "tokenize valid types" $ do
+            L.scanTokens "earth fire water air" `shouldBe` ([],[TK.TKearth {TK.pos = (1,1)},TK.TKfire {TK.pos = (1,7)},TK.TKwater {TK.pos = (1,12)},TK.TKair {TK.pos = (1,18)}])
+
+        it "parses multi word token, no matter how many white spaces in between" $ do
+            L.scanTokens "is mastered by" `shouldBe` ([],[TK.TKmasteredBy {TK.pos = (1,1)}])
+            L.scanTokens "is     mastered by" `shouldBe` ([],[TK.TKmasteredBy {TK.pos = (1,1)}])
+            L.scanTokens "is     mastered     by" `shouldBe` ([],[TK.TKmasteredBy {TK.pos = (1,1)}])
+            L.scanTokens "is mastered     by" `shouldBe` ([],[TK.TKmasteredBy {TK.pos = (1,1)}])
