b03e9f5a17f21033a532839acc12db0122a4fe74
neilvillamizar <neilvillamizar@gmail.com>
2021-06-08 19:27:15 -0400

    show Position imp

--- language-bender/src/FrontEnd/Utils.hs
+++ language-bender/src/FrontEnd/Utils.hs
@@ -3,4 +3,8 @@
 module FrontEnd.Utils where
 
 -- Token position in file
-data Position = Position {row :: Int, col :: Int} deriving (Eq, Show)
+data Position = Position {row :: Int, col :: Int} deriving (Eq)
+
+instance Show Position where
+	show (Position r c) =
+		" line " ++ (show r) ++ ", column " ++ (show c) ++ "."
