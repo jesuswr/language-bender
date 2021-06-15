7f72905b02fdfc42319a1696e0108634d4973b17
Luis Diaz <ldiazn98@gmail.com>
2021-06-14 22:31:54 -0400

    added some comments; refactor

--- language-bender/src/FrontEnd/SymTable.hs
+++ language-bender/src/FrontEnd/SymTable.hs
@@ -1,23 +1,38 @@
+{-
+    Data Structures and functions for the symbol table
+-}
+
 module FrontEnd.SymTable where
 
---import Control.Monad.State
-import qualified Control.Monad.Trans as T
-import qualified Control.Monad.RWS   as RWS
+
+-- <Language Bender Imports> ------------------------------------
 import qualified FrontEnd.Errors     as E
 import qualified FrontEnd.AST        as AST
 
+-- <Utility Data types> -----------------------------------------
+import qualified Control.Monad.Trans as T
+import qualified Control.Monad.RWS   as RWS
 import qualified Data.Map.Strict     as Map
---import qualified Data.HashMap.Strict as H
+-----------------------------------------------------------------
 
-
-type Scope = Int
+-- <Type Definitions> ------------------------------------------
+-- | Execution context in the program
+type Scope = Int 
+-- | Stack of currently available scopes 
 type ScopeStack = [Scope]
-type Identifier = String
+-- | name of a symbol  
+type Identifier = String                        
+-- | Map from ids to its corresponding symbol data. Multiple symbols may have the same name, that's why we keep a 
+--   list of Symbols instead of a single symbol
+type Dictionary = Map.Map Identifier [Symbol] 
+-- | State used to simulate an imperative process of type checking 
+type LangBendState a = RWS.RWST () [E.Error] SymTable a
 
 
-data Category
+-- | Symbol Type with its corresponding data
+data SymType   
     = Variable      { varType ::  Maybe AST.Type, initVal :: Maybe AST.Expr }
-    | Constant      { varType ::  AST.Type, initVal :: AST.Expr }
+    | Constant      { consType ::  AST.Type, consInitVal :: AST.Expr }
     | Type          { unType :: AST.Type}
     | Procedure     { args :: [AST.FuncArg], body :: AST.Expr }
     | Function      { args :: [AST.FuncArg], retType :: AST.Type , body :: AST.Expr }
@@ -26,22 +41,20 @@ data Category
     | Reference     { refName :: AST.Name, refType :: AST.Type }
     deriving (Eq, Show)
 
-
-data SymValue = SymValue
-    { identifier :: Identifier          -- Entry name
-    , category :: Category              -- Category of the entry
-    , scope :: Scope                    -- current scope at insertion
-    , enrtyType :: (Maybe SymValue)     -- pointer to another entry
+-- | Symbol Data type
+data Symbol = Symbol
+    { identifier :: Identifier          -- ^ Entry name
+    , symType :: SymType                -- ^ Symbol Type of the entry
+    , scope :: Scope                    -- ^ current scope at insertion
+    , enrtyType :: Maybe SymType        -- ^ pointer to another entry
     } deriving (Eq, Show)
 
-type Dictionary = Map.Map Identifier [SymValue]
-
+-- | Symbol Table: Le Blanc - Cook based implementation 
 data SymTable = SymTable
-    { stDict :: Dictionary              -- dict of valid symbols
-    , stScopeStk :: ScopeStack          -- stack of scopes
-    , stCurrScope :: Int                -- curr scope 
+    { stDict :: Dictionary              -- ^ dict of valid symbols
+    , stScopeStk :: ScopeStack          -- ^ stack of scopes
+    , stNextScope :: Int                -- ^ current scope 
     } deriving (Eq, Show)
 
 
-type LangBendState RWS.RWST () [E.Error] SymTable IO
 
