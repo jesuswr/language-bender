d37dc063e96b1f218f550d22e000431312eb4dfe
Luis Diaz <ldiazn98@gmail.com>
2021-07-22 22:35:54 -0400

    added type checking for variable declaration, for function return and body; added type checking functions

--- language-bender/src/FrontEnd/ParserCheck.hs
+++ language-bender/src/FrontEnd/ParserCheck.hs
@@ -54,11 +54,6 @@ preCheckDecls :: AST.Declaration -> ParserState ()
 
 preCheckDecls f@AST.Func {AST.decName=_decName, AST.args=_args, AST.retType=_retType, AST.body=_body} = do
 
-    -- check valid return type if needed 
-    M.when (isJust _retType) $ do
-        let Just t = _retType
-        M.void $ checkType t
-
     -- Create a new symbol for this function 
     let symbol  = declToSym f
 
@@ -82,7 +77,7 @@ preCheckFunArgs _args = do
     let variables = [
                 ST.Symbol {
                     ST.identifier=_argName,
-                    ST.symType= ST.Variable{ST.varType= _argType, ST.initVal=_defaultVal, ST.isConst = False} ,
+                    ST.symType= ST.Variable{ST.varType= _argType, ST.initVal=_defaultVal, ST.isConst = False},
                     ST.scope=0,
                     ST.enrtyType=Nothing
                 }
@@ -119,6 +114,9 @@ checkDecls v@AST.Variable{ AST.decName = sid, AST.varType =  t, AST.initVal = iv
 
     -- Add new variable to symbol table 
     tryAddSymbol newSym
+
+    -- check initial value if provided 
+    M.forM_ ival (_checkTypeMatch'' t)
 
     return v
 
@@ -191,25 +189,11 @@ checkDecls s@AST.Struct {AST.decName=_decName, AST.fields=_fields} = do
 -- Check function declaration
 checkDecls f@AST.Func {AST.decName=_decName, AST.args=_args, AST.retType=_retType, AST.body=_body} = do
 
+    -- Check if function body matches return type
+    _checkTypeMatch'' _retType _body
 
-    let bodyType = AST.expType _body
-        
-        checkFuncType (Just t)
-            | bodyType == t = Just bodyType       -- types of firm and body are equal
-            | otherwise     = Just AST.TypeError  -- type error
-        checkFuncType Nothing = Just bodyType     -- Inferred type
+    return f
 
-        funcType = checkFuncType _retType
-
-        f' = f{AST.retType=funcType}
-
-    -- Create a new symbol for this function 
-        symbol  = declToSym f'
-
-    -- try to add function symbol 
-    tryAddSymbol symbol -- check if redefined
-
-    return f'
 
 -- | Check if a given expression uses valid names only
 checkExpr :: AST.Expr -> ParserState AST.Expr
@@ -524,7 +508,7 @@ declToSym decl = ST.Symbol {
         declToSymType AST.Func {AST.args=_args, AST.retType=_retType, AST.body=_body} =
             ST.Function {
                 ST.args=_args,
-                ST.retType=fromMaybe AST.TUnit  _retType,
+                ST.retType=_retType,
                 ST.body=_body
             }
 
@@ -575,3 +559,30 @@ checkIdIsVarOrReference name = do -- Check that given name is a valid one and it
                 Just sym -> M.unless ( ST.isVariable  sym || ST.isReference  sym) $
                             addStaticError SE.NotAValidVariable {SE.symName=name, SE.actualSymType=ST.symType sym}
                 _        -> return ()
+
+
+
+-- < Utility functions to check matching types > ---------------------------------------------------- 
+
+-- | Check if a given expr typematchs an expected set of types, and report error if they don't
+_checkTypeMatch :: [AST.Type] -> AST.Expr -> ParserState Bool
+_checkTypeMatch expected  = _checkTypeMatch' expected . AST.expType  
+
+-- | Check if a given type matches some of the expected ones 
+_checkTypeMatch' :: [AST.Type] -> AST.Type -> ParserState Bool
+_checkTypeMatch' expected exprType = do
+    -- Check if current type matches one of expected types
+    case exprType of
+        AST.TypeError -> return False
+        t             -> if t `elem` expected
+                            then return True
+                            else do
+                                addStaticError $ SE.UnmatchingTypes expected exprType
+                                return False
+
+_checkTypeMatch'' :: AST.Type -> AST.Expr -> ParserState Bool
+_checkTypeMatch'' t = _checkTypeMatch [t]
+
+-- | Check if a sorted list of expression matches a sorted list of types
+_checkTypeMatchesArgs :: [[AST.Type]] -> [AST.Expr] -> ParserState Bool
+_checkTypeMatchesArgs expected args = M.zipWithM _checkTypeMatch expected args <&> and
